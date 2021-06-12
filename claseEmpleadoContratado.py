1645c9f4dfb6e5e0d5899b59630e99203b7a9650
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2021-06-12 14:21:57 -0300

    Update claseEmpleadoContratado.py

--- claseEmpleadoContratado.py
+++ claseEmpleadoContratado.py
@@ -11,12 +11,14 @@ class EmpleadoContratado(Empleado):
         super().__init__(docu, nom, direc, tel)
         self.__fechainicio = fechaini
         self.__fechafin = fechafin
-        self.__cantHoras = cahoras
-        self.__sueldo = float(self.__cantHoras * self.__valorHora)
-    def SetValorHora(self):
+        if type(cahoras) is int:
+            self.__cantHoras = cahoras
+        self.__sueldo = self.__cantHoras * self.__valorHora
+    @classmethod
+    def SetValorHora(cls):
         val = float(input('Ingrese nuevo valor por hora:'))
-        self.__valorHora = val
-        print('Valor por hora actualizado. Nuevo valor:',self.__valorHora)
+        cls.__valorHora = val
+        print('Valor por hora actualizado. Nuevo valor:',cls.__valorHora)
     def SetCantidadHoras(self,cant):
         self.__cantHoras += cant
         print('Se ha actualizado la cantidad de horas trabajadas.')
@@ -25,6 +27,7 @@ class EmpleadoContratado(Empleado):
     def ActualizaSueldo(self):
         self.__sueldo = self.__cantHoras * self.__valorHora
     def GetSueldo(self):
+        self.ActualizaSueldo()
         return self.__sueldo
     def GetFechaInicio(self):
         return self.__fechainicio
@@ -32,5 +35,6 @@ class EmpleadoContratado(Empleado):
         return self.__fechafin
     def GetCantidadHoras(self):
         return self.__cantHoras
-    def GetValorHora(self):
-        return self.__valorHora
+    @classmethod
+    def GetValorHora(cls):
+        return cls.__valorHora
